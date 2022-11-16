d65508f862ab179e1544a8c4743216f68e8bfe9b
Maciej <mtrzsalski@gmail.com>
2022-11-16 15:21:39 +0100

    add videos and animations

--- src/Content.js
+++ src/Content.js
@@ -21,12 +21,31 @@ export default function Content( {changeScrollDistanceFromTop} ) {
                 <p>Hobby 3d Artist</p>
                 <p>Tech Entusiast</p>
             </div>
-            <div className='element'></div>
-            <div className='element'></div>
-            <div className='element'></div>
-            <div className='element'></div>
-            <div className='element'></div>
-            <div className='element'></div>
+            <div className='element long'></div>
+            <div className='element long'></div>
+            <div className='element long'></div>
+            <div className='element long'></div>
+            <div className='element gigalong left'>
+                <p>Hi!</p>
+                <p>I'm a Software Engineer from Poland who's passionate about making beautiful looking web pages.</p>
+                <p>I've been working as a Software Engineer for a year and I'm looking forward to learning more.</p>
+            </div>
+            <div className='element gigalong'></div>
+            <div className='element gigalong'></div>
+            <div className='element gigalong'></div>
+            <div className='element gigalong'></div>
+            <div className='element gigalong right'>
+                <p>All of the models on this page were made by me using Blender. This is really what my room looks like.</p>
+                <p>Well only difference is that in reality I don't have that picture on the wall but I thought it looked empty.</p>
+            </div>
+            <div className='element gigalong'></div>
+            <div className='element gigalong'></div>
+            <div className='element gigalong left'>
+                <p>Project heavily inspired by <a href="http://bokoko33.me">this</a> portfolio</p>
+                <p>Find me on LinkedIn <a href="linkedin.com">LinkedIn</a></p>
+                <p>Visit me on Github <a href="github.com">Github</a></p>
+            </div>
+            <div className='element gigalong'></div>
         </div>
     );
 }
--- src/Room.js
+++ src/Room.js
@@ -1,15 +1,23 @@
-import React, { Suspense, useRef } from 'react';
+import React, { Suspense, useEffect, useRef } from 'react';
 import {Model}  from './RoomFinalTexture';
 import { Box } from '@react-three/drei/core';
-import { useFrame, useThree } from '@react-three/fiber'
 import { OrbitControls, OrthographicCamera } from '@react-three/drei';
 
 export default function Room( {scrollDistanceFromTop} ) {
     const ref = useRef();
-    const { viewport } = useThree()
-    useFrame(() => {
-        ref.current.zoom = 60 + scrollDistanceFromTop * 20;
-    })
+
+    const getZoomValue = (scrollDistance) => {
+        return scrollDistance < 1.63 ? scrollDistance : 1.63
+    }
+
+    const getZoomStartingValue = () => {
+        return window.innerWidth < 680 ? 31 : 62; 
+    }
+
+    useEffect(() => {
+        ref.current.zoom = getZoomStartingValue() + getZoomValue(scrollDistanceFromTop) * 50;
+    }, [scrollDistanceFromTop])
+
     return(
         <>
             <OrbitControls enabled={false} />
@@ -17,13 +25,12 @@ export default function Room( {scrollDistanceFromTop} ) {
             <directionalLight position={[4, 10, 6]} shadow-mapSize-width={2048} shadow-normalBias={0.05}
             shadow-mapSize-height={2048} intensity={2} shadow-camera-left={-30} shadow-camera-right={30} shadow-camera-top={30} shadow-camera-bottom={-30}
             shadow-camera-near={0.1} shadow-camera-far={60} castShadow/>
-            <OrthographicCamera ref={ref} zoom={70} makeDefault position={[10, 10, 10]}/>
-            {/* <gridHelper args={[20, 20, `white`, `gray`]} /> */}
-            <Box receiveShadow castShadow position={[4, -2, 4]} scale={[50, 0.2, 50]}>
+            <OrthographicCamera ref={ref} far={70} near={0.1} makeDefault position={[15, 15, 15]}/>
+            <Box receiveShadow castShadow position={[4, -2, 4]} scale={[100, 0.2, 100]}>
             <meshPhysicalMaterial color="hotpink"/>
             </Box>
             <Suspense fallback={null}>
-            <Model scale={viewport.width < 12 ? 0.4: 0.9} position={[0,0,0]} rotation={[0,1.5 * Math.PI,0]}/>
+            <Model scrollDistanceFromTop={scrollDistanceFromTop}/>
             </Suspense>
         </>
     )
